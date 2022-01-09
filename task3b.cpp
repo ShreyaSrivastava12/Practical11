169d91e21c02ff95fc0dcce0fdeb8911829978b5
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-01-09 18:43:27 +0530

    Create task3b.cpp
    Solution of Diamond Problem Using Virtual Base Classes

---
+++ task3b.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class a {
+      public:
+      void display()
+      {
+          cout<<endl;
+          cout<<"This the display function of the base class a\n";
+      }
+};
+class b : virtual public a{
+      public:
+      void display1()
+      {
+          cout<<"This the display1 function of the child class b\n";
+      }
+};
+class c : virtual public a{
+      public:
+      void display2()
+      {
+          cout<<"This the display2 function of the child class c\n";
+      }
+};
+class d : public b , public c{
+      public:
+      void display3()
+      {
+          cout<<"This the display1 function of the child class d\n";
+      }
+};
+int main()
+{
+     d obj; 
+
+     obj.display(); 
+     cout<<endl;
+
+     obj.display1();
+     cout<<endl;
+
+     obj.display2();
+     cout<<endl;
+}
