751607a59a76f732821d8e7b521e5db73559f67c
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-01-09 18:22:32 +0530

    Create task2c.cpp
    Function of blue whale by object of blue whale

---
+++ task2c.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class mammal{
+    public :
+    void Display1()
+    {
+        cout<<"I am Mammal"<<endl;
+    }
+};
+class marineAnimal{
+    public :
+    void Display2()
+    {
+        cout<<"I am a Marine Animal "<<endl;
+    }
+};
+class blueWhale : public mammal , public marineAnimal{
+    public :
+    void Display3()
+    {
+        cout<<"I belong to both the categories : Mammals and Marine Animals"<<endl;
+    }
+};
+int main()
+{
+    mammal o1;
+
+    marineAnimal o2;
+
+    blueWhale o3;
+
+    o3.Display3();
+    cout<<endl;
+}
